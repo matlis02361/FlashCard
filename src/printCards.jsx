0d22d721a4034b0ac40c85d41f9d892b4a643c21
Mateusz <matlis02361@gmail.com>
2022-12-10 01:57:44 +0100

    @media print & PrintStyle disappointment ;)

--- src/printCards.jsx
+++ src/printCards.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
+import "./scss/App.scss";
 
 export const PrintCards = () => {
   const printWindow = () => {
     const printContents = document.getElementById("printCards").innerHTML;
     const originalContents = document.body.innerHTML;
     document.body.innerHTML = printContents;
+
+    // Dodaj kod @media print tutaj
+    const printStyle = document.createElement("style");
+    printStyle.textContent = `
+  @media print {
+    .print-cards {
+      display: flex !important;
+      flex-wrap: wrap !important;
+      justify-content: center !important;
+    }
+
+    .print-cards .card {
+      width: 200px !important;
+      height: 100px !important;
+      margin: 5px !important;
+      background: black !important;
+    }
+  }
+`;
+    document.head.appendChild(printStyle);
+
     window.print();
     document.body.innerHTML = originalContents;
   };
